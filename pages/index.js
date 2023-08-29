5581f58b9971593f33484538f4d2a63670469a6f
Lucas Santana <santanafx@hotmail.com>
2023-08-29 08:15:48 -0300

    feat: detalhes dos pokemons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image'
-import Card from './components/Card'
+import Card from '../components/Card'
 
 export async function getStaticProps() {
   const maxPokemons = 251
@@ -8,8 +8,6 @@ export async function getStaticProps() {
 
   const res = await fetch(`${api}/?limit=${maxPokemons}`)
   const data = await res.json()
-
-  //add pokemon index
 
   data.results.forEach((item, index) => {
     item.id = index + 1
