dd0443da4d859864bdd71e9b0ff3d7e74bbb6b33
Lucas Santana <santanafx@hotmail.com>
2023-08-29 07:18:20 -0300

    style: estilização dos cards de pokemon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import Card from './components/Card'
 
 export async function getStaticProps() {
   const maxPokemons = 251
@@ -22,12 +24,23 @@ export async function getStaticProps() {
 
 export default function Home({ pokemons }) {
   return (
-    <div>
-      <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span className={styles.title_span}>Next</span>
+          <Image
+            src="/images/pokeball.png"
+            width="50"
+            height="50"
+            alt="PokeNext"
+          />
+        </h1>
+      </div>
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   )
 }
